87ffd2e3f3888429c58b00ff944b52bb5fde229e
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-09-07 16:24:25 +0900

    기초 설정

---
+++ src/test/java/com/example/stepbackend/StepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stepbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
