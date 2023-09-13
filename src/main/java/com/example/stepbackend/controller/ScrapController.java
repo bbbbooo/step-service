e3bb76b43505471edea5e736b3dc8ec455b22763
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-09-13 14:39:57 +0900

    [Refactor] : 나만의 문제집 생성 리팩토링

--- src/main/java/com/example/stepbackend/controller/ScrapController.java
+++ src/main/java/com/example/stepbackend/controller/ScrapController.java
@@ -1,6 +1,5 @@
 package com.example.stepbackend.controller;
 
-import com.example.stepbackend.aggregate.dto.scrap.ScrapDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
