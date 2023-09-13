2ed91cde85f794263e27214e6115c7102b6fe730
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-09-13 14:10:40 +0900

    [Refactor] : 나만의 문제집 생성 리팩토링

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -36,10 +36,3 @@ VALUES (1, '1, 2, 3, 4, 5', true),
        (1, '6, 7, 8, 9, 10', false);
 
 
-INSERT INTO user (uid, created_date, email, nickname, profile_image, provider, role)
-VALUES
-    ('dummy_user1@example.com', '2023-09-12 10:00:00.000000', 'dummy_user1@example.com', 'KakaoUser1', 'profile1.jpg', 'KAKAO', 'USER');
-
-INSERT INTO token (access_token, user_id)
-VALUES
-    ('eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE2OTQ1ODAxODMsImV4cCI6MTY5NDU5MDE4M30.Q0Z0pvTT-G9ivPeODQeBCvE7psIM7yyMN08_DLY1cNY', 1);
