b94e650da59d5bc54696c292faf1e1a706204d5a
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-09-16 20:25:53 +0900

    [Feat] : 나만의 문제집 공유 게시글 테스트 코드 구현

---
+++ src/main/java/com/example/stepbackend/aggregate/dto/board/CreateBoardRequestDTO.java
@@ -0,0 +1,16 @@
+package com.example.stepbackend.aggregate.dto.board;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class CreateBoardRequestDTO {
+    private String workBookName;
+
+    private String description;
+
+    private Long workBookNo;
+}
