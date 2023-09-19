fa80bf26cbdc0142a3fb63851c9d5e77a033055b
후니 <pyunghun120@gmail.com>
2023-09-19 20:42:32 +0900

    [Feat, Test] User,Security 관련 test 추가

--- src/main/java/com/example/stepbackend/global/security/controller/AuthController.java
+++ src/main/java/com/example/stepbackend/global/security/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.stepbackend.global.security.controller;
 
+import com.example.stepbackend.aggregate.dto.user.CreateUserDTO;
+import com.example.stepbackend.aggregate.dto.user.SignUpDTO;
 import com.example.stepbackend.global.common.annotation.CurrentUser;
 import com.example.stepbackend.global.common.response.api.ApiResponse;
 import com.example.stepbackend.global.security.dto.AuthResponse;
@@ -11,6 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -58,4 +61,39 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 
+    @GetMapping("/signin")
+    public String kakaoLogin(String code) {
+        return null;
+    }
+
+    @PostMapping ("/signup")
+    public ModelAndView signup(@RequestBody SignUpDTO signUpDTO) {
+
+        // 1. 회원정보 받기, form, json
+        System.out.println("sigupDTO" + signUpDTO);
+        // 2. 회원정보를 token으로 말기
+
+        // 3. 회원정보를 db에 저장
+
+        // 4. token으로 만 정보(access_token) db에 저장
+
+        // 5. 로그인
+
+        // 6. 로그인이 성공하면 토큰을 localStorage or cookie에 담아준다
+
+        // 7. 매 요청마다 헤더에 토큰을 담아준다
+
+        // 7-1. 회원정보를 토큰에서 파싱해야 되는 경우면 jwt.decoded(); *라이브러리 다운로드 필요
+
+        return null;
+    }
+
+    @GetMapping("/mypage")
+    public ModelAndView mypage(@CurrentUser UserPrincipal userPrincipal, ModelAndView mv) {
+
+
+        mv.setViewName("/auth/mypage");
+
+        return mv;
+    }
 }
