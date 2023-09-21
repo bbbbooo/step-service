71f429f20f9bdf3a07870276be83af3d965f502d
후니 <pyunghun120@gmail.com>
2023-09-21 10:17:52 +0900

    [Feat, Test] User,Security 관련 test 추가

--- src/main/java/com/example/stepbackend/global/security/controller/AuthController.java
+++ src/main/java/com/example/stepbackend/global/security/controller/AuthController.java
@@ -9,17 +9,24 @@ import com.example.stepbackend.global.security.dto.AuthResponseBody;
 import com.example.stepbackend.global.security.service.IssueTokenService;
 import com.example.stepbackend.global.security.token.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 public class AuthController {
 
@@ -29,6 +36,16 @@ public class AuthController {
     public AuthController(IssueTokenService issueTokenService) {
         this.issueTokenService = issueTokenService;
     }
+
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String clientSecret;
 
     @PostMapping("/token")
     public ResponseEntity<?> issueToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken) {
@@ -51,7 +68,7 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.CREATED).body(authResponse);
     }
 
-    @GetMapping("name")
+    @GetMapping("/name")
     public ResponseEntity<?> issueToken(@CurrentUser UserPrincipal userPrincipal) {
         System.out.println("userPrincipal = " + userPrincipal);
         System.out.println("userPrincipal.getNickname() = " + userPrincipal.getNickname());
@@ -61,39 +78,93 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 
-    @GetMapping("/signin")
-    public String kakaoLogin(String code) {
-        return null;
+    @GetMapping("/signup")
+    public String kakaoSignUp() {
+
+        return "auth/signup";
     }
 
-    @PostMapping ("/signup")
-    public ModelAndView signup(@RequestBody SignUpDTO signUpDTO) {
+    @GetMapping("/access-permit")
+    public String accessPermit(@RequestParam String code, HttpServletResponse response) {
 
-        // 1. 회원정보 받기, form, json
-        System.out.println("sigupDTO" + signUpDTO);
-        // 2. 회원정보를 token으로 말기
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.valueOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
 
-        // 3. 회원정보를 db에 저장
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("code", code);
+        parameters.add("client_id", clientId);
+        parameters.add("redirect_uri", redirectUri);
+        parameters.add("client_secret", clientSecret);
+        parameters.add("grant_type", "authorization_code");
 
-        // 4. token으로 만 정보(access_token) db에 저장
+        HttpEntity<MultiValueMap> entity = new HttpEntity<>(parameters, headers);
 
-        // 5. 로그인
+        String uri = "https://kauth.kakao.com/oauth/token";
 
-        // 6. 로그인이 성공하면 토큰을 localStorage or cookie에 담아준다
+        RestTemplate restTemplate = new RestTemplate();
 
-        // 7. 매 요청마다 헤더에 토큰을 담아준다
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
 
-        // 7-1. 회원정보를 토큰에서 파싱해야 되는 경우면 jwt.decoded(); *라이브러리 다운로드 필요
+        ResponseEntity<Object> res = restTemplate.exchange(uri, HttpMethod.POST, entity, Object.class);
 
-        return null;
+        Map<String,Object> body = (Map<String, Object>) res.getBody();
+        String accessToken = (String) body.get("access_token");
+
+        Cookie cookie = new Cookie("authorize-access-token", accessToken);
+        response.addCookie(cookie);
+
+        return "redirect:/auth/signup";
+    }
+
+    @GetMapping("/token")
+    public void getToken(@RequestBody Map<String, Object> token) {
+        System.out.println("token = " + token);
+    }
+
+    @GetMapping("/signin")
+    public String kakaoLogin() {
+
+
+
+        return "auth/signin";
     }
 
     @GetMapping("/mypage")
-    public ModelAndView mypage(@CurrentUser UserPrincipal userPrincipal, ModelAndView mv) {
+    public String mypage() {
 
 
-        mv.setViewName("/auth/mypage");
 
-        return mv;
+        return "auth/mypage";
     }
+
+//    @PostMapping ("/signup")
+//    public ModelAndView signup(@RequestBody SignUpDTO signUpDTO) {
+//
+//        // 1. 회원정보 받기, form, json
+//        System.out.println("sigupDTO" + signUpDTO);
+//        // 2. 회원정보를 token으로 말기
+//
+//        // 3. 회원정보를 db에 저장
+//
+//        // 4. token으로 만 정보(access_token) db에 저장
+//
+//        // 5. 로그인
+//
+//        // 6. 로그인이 성공하면 토큰을 localStorage or cookie에 담아준다
+//
+//        // 7. 매 요청마다 헤더에 토큰을 담아준다
+//
+//        // 7-1. 회원정보를 토큰에서 파싱해야 되는 경우면 jwt.decoded(); *라이브러리 다운로드 필요
+//
+//        return null;
+//    }
+
+//    @GetMapping("/mypage")
+//    public ModelAndView mypage(@CurrentUser UserPrincipal userPrincipal, ModelAndView mv) {
+//
+//
+//        mv.setViewName("/auth/mypage");
+//
+//        return mv;
+//    }
 }
